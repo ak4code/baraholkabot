89d5b7928f48b44476745ddffe988ec5a1057978
kharitonov.dev <haritonov.aka@gmail.com>
2021-02-11 01:30:46 +0300

    dev: fix font

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import telebot
 import textwrap
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 bot = telebot.TeleBot(os.getenv("TOKEN"))
@@ -10,7 +11,7 @@ bot = telebot.TeleBot(os.getenv("TOKEN"))
 def baner_maker(text):
     im = Image.open("./template.jpg")
     xsize, ysize = im.size
-    font = ImageFont.truetype("./fonts/Roboto-Medium.ttf", 70)
+    font = ImageFont.truetype("./fonts/Symbola.ttf", 70, encoding="unic")
     d = ImageDraw.Draw(im)
     text = textwrap.wrap(text, width=26)        
     d.multiline_text((xsize / 2, ysize / 2), text="\n".join(text), fill="black", anchor="mm", font=font, align="center")
